f445262e219f8c8c208e432177230029b951adac
Andreas Grønlien <andreas@gronlien.no>
2017-09-14 11:06:20 +0200

    flere kommentarer

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-// Global variables
+// Globale variables
+
+// Siden URL
+var options = "";
+
+// Brukervariabler
 var usertype = 0;
 var username = "NONAME";
-var options = "";
 var userid = 0;
+
+// Variable som refererer til siden sinnhold
 var listofconcerts = [];
 var listoftechnicians = [];
 
 // Functions to run after DOM is ready
 $(document).ready(function(){
-    // Initialization
+    // Initsiering
+
+    // Vi lagrer url-en i options-stringen slik at du vi kan lese den senere
     options = parseUrl(window.location.href);
+        // Regex for å finne ut om ?debug-kommandoen er inkludert i URL
         if (/debug/i.test(options)) {
             $(".debug").show();
         }
+    // Oppdater skjermen så vi får med alle ajax-kall
     redraw();
 
     // Database queries
 
+    // Lager et html-element med teknikere som hører til en konsert
     function getListOfTechnicians(concertID) {
         // TODO database-call: (userid,concertID)
         let l = ["Jens", "Nils", "Truls"];
+
+        // Vi bygger HTML-element
         let str = "<ul class='technicianlist'>";
         for (i in l) {
             str+= "<li>"+l[i]+"</li>";
         }
         str+="</ul>";
-        console.log(str);
         return str;
     }
 
+    // Lager et html-element med konserter
     function getListOfConcertes() {
         // TODO: database-call: (userid)
         let l = []
@@ -44,10 +57,10 @@ $(document).ready(function(){
             str+= "<li><span>"+l[i]+"</span><button class='concert_button' id='"+concertID+"'>Mer info</button>"+"</li>";
         }
         str+="</ul>";
-        console.log(str);
         return str;
     }
 
+    // Lager et html-element med informasjon om en konsert
     function getConcertInfo(concertID) {
         // TODO: database-call(userid, concertID)
         let str ="<div>";
@@ -64,20 +77,20 @@ $(document).ready(function(){
 
     // FUNCTIONS
 
-    // Calls ajax when page should change content
+    // Tegner siden på nytt etter brukertype
     function redraw() {
         switch(usertype) {
-            case 0:
+            case 0: // Ikke pålogget
                 $.ajax({url: "no_user.html",dataType: 'html', success: function(result){
                     $("#root").html(result);
                 }});
                 break;
-            case 1:
+            case 1: // Bruker er arrangør
                 $.ajax({url: "arrang.html",dataType: 'html', success: function(result){
                         $("#root").html(result);
                 }});
                 break;
-            case 2:
+            case 2: // Bruker er teknikker
                 $.ajax({url: "tekni.html",dataType: 'html', success: function(result){
                     $("#root").html(result);
                 }});
@@ -88,18 +101,18 @@ $(document).ready(function(){
         console.log("Pagestate:"+usertype);
     }
 
-    // Returns the url of the web-page
+    // Finner sidens URL-addresse
     function parseUrl( url ) {
         var a = document.createElement('a');
         a.href = url;
         return a;
     } 
 
-    // Captures logon information from input fields and should call database
+    // Henter informasjon fra bruker- og passord-felt og prøver å logge inn
     function logon() {
         username = $("#username_field").val();
         console.log("Username "+username);
-        // Debug: random usertype
+        // TODO
         if (username.charAt(0) == 'a') {
             usertype = 1;
         } else if (username.charAt(0) == 'b') {
@@ -108,7 +121,7 @@ $(document).ready(function(){
         redraw();
     }
 
-    // Logs out by reloading page
+    // Logger ut, for nå så laster den bare siden på nytt
     function logout() {
         console.log("Logout");
         location.reload();
@@ -116,45 +129,46 @@ $(document).ready(function(){
 
     // EVENTS
 
-    // Debug buttons
+    // Debug-knapper
     $(".debug_button").click(function() {
         usertype = parseInt(this.id);
         redraw();
     });
 
-    // Capture 'enter'-key from username-field
+    // Fang 'enter'-trykk fra brukernavn-feltet
     $('body').on('keyup', "#username_field", function (e) {
         if (e.keyCode === 13) {
            logon();
         }
     });
 
-    // Capture 'ener'-key from password-field
+    // Fang 'enter'trykk fra passord-feltet
     $('body').on('keyup', "#password_field", function (e) {
         if (e.keyCode === 13) {
            logon();
         }
     });
 
-    // Hook up login-button
+    // Fant trykk på påloggingsknappen
     $('body').on('click', ".login_button", function () {
         logon();
     });
 
-    // Hook up logout-button
+    // Fang trykk på avloggingsknappen
     $('body').on('click', ".logout_button", function () {
         logout();
     });
 
-    // Hook up more info on concert-button
+    // Fang trykk på knapp for mer informasjon om konsert
     $('body').on('click', ".concert_button", function () {
         let concertID = parseInt(this.id);
         console.log("Get concert info");
         $(this).after(getConcertInfo(concertID));
+        // Denne funksjonen skjuler bare knappen etter å ha blitt kallet, kan alltids forbedres
         $(this).css("display","none");
     });
 
-    // Correctly inject content into elements after ajax is complete
+    // VIKTIG FUNKSJON: Kan injsere innhold i DOM-treet etter ajax-oppdatering.
     $(document).ajaxComplete(function() {
         $('#username').html(username);
         $('#listofconcerts').html(getListOfConcertes());
