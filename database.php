b181c7fb26dd667fbf6dd22aa0bd51ce0a0d066a
kronen77 <even__kj@hotmail.com>
2017-09-19 12:12:03 +0200

    endret til object-oriented metode

--- database.php
+++ database.php
@@ -1,15 +1,17 @@
 <?php
-	$db = mysqli_connect("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
+	$db = new mysqli("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
 	if ($db->connection_error){
 		die("Connection Failed: " . $db->connection_error);
 	}
-	$bandNavn = $mysqli_query($db, "SELECT" * FROM band"; //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
+	$bandNavn = $mysqli->query("SELECT navn FROM band"); //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
 	
 	
 	/*ser ntnu selv viser gammelt format så bruker det de anbefaler. Kommentert ut kode er kode for hvordan nettsider mener det skal gjøres nå. Hvis det som står over
 	fungerer - slett utkommentert kode.
+	
 	$db = new mysqli("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
-	$bandNavn = $mysqli->query("SELECT navn FROM band") //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
+	$bandNavn = $mysqli_query($db, "SELECT" * FROM band";
+	
 	
 	Vi må bare se hvilken som virker */
 ?>
