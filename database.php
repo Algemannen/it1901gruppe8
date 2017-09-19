53fb9b418467e61f8f2203757c0f2978b0b0096b
kronen77 <even__kj@hotmail.com>
2017-09-19 11:14:12 +0200

    Funksjon for hvorfan lagre info i variabel php

--- database.php
+++ database.php
@@ -1,3 +1,6 @@
 <?php
-$connect = new mysqli("https://mysqladmin.stud.ntnu.no/", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
+	$mysqli = new mysqli("https://mysqladmin.stud.ntnu.no/", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
+
+	$bandNavn = $mysqli->query("SELECT navn FROM band") //
+
 ?>
