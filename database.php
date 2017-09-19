f37cfe112db5e1fca72aa80a5e97023d47ebd623
kronen77 <even__kj@hotmail.com>
2017-09-19 11:20:29 +0200

    glemt lukking av php

--- database.php
+++ database.php
@@ -2,3 +2,4 @@
 	$mysqli = new mysqli("https://mysqladmin.stud.ntnu.no/", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
 
 	$bandNavn = $mysqli->query("SELECT navn FROM band") //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
+?>
