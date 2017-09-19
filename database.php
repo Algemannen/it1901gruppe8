0fb7988145cf48683758324456a8357c3f57703b
kronen77 <even__kj@hotmail.com>
2017-09-19 11:34:07 +0200

    github aksepterte ikke multiline comment, byttet til single line for oversikt

--- database.php
+++ database.php
@@ -3,10 +3,10 @@
 	$bandNavn = $mysqli_query($db, "SELECT" * FROM band"; //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
 	
 	
-	/* ser ntnu selv viser gammelt format så bruker det de anbefaler. Kommentert ut kode er kode for hvordan nettsider mener det skal gjøres nå. Hvis det som står over
-	fungerer - slett utkommentert kode.
-	$db = new mysqli("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
-	$bandNavn = $mysqli->query("SELECT navn FROM band") //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
+	//ser ntnu selv viser gammelt format så bruker det de anbefaler. Kommentert ut kode er kode for hvordan nettsider mener det skal gjøres nå. Hvis det som står over
+	//fungerer - slett utkommentert kode.
+	//$db = new mysqli("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
+	//$bandNavn = $mysqli->query("SELECT navn FROM band") //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
 	
-	Vi må bare se hvilken som virker */
+	//Vi må bare se hvilken som virker 
 ?>
