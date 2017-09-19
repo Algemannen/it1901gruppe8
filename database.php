06be578b4b9ceb3372177083fdfa294021c5c4a2
kronen77 <even__kj@hotmail.com>
2017-09-19 11:16:19 +0200

    Lagt inn beskrivelse av funksjon

--- database.php
+++ database.php
@@ -1,6 +1,4 @@
 <?php
 	$mysqli = new mysqli("https://mysqladmin.stud.ntnu.no/", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
 
-	$bandNavn = $mysqli->query("SELECT navn FROM band") //
-
-?>
+	$bandNavn = $mysqli->query("SELECT navn FROM band") //Funksjon for hvordan lagre bandnavn fra liste band inn i en variabel $bandNavn
