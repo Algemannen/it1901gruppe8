2c61f9bb88654ab0c7193c48c1ded6d6b340998e
miriamsl <miriamsl@stud.ntnu.no>
2017-09-21 09:18:05 +0200

    Lagt til kommentarer i php-fila, som beskriver koden.

--- database.php
+++ database.php
@@ -1,26 +1,26 @@
 <?php
-	$dbconn = new mysqli("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
+	$dbconn = new mysqli("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival"); //Oppkobling til database
 	if ($dbconn->connect_error){
 		die("Connection Failed: " . $dbconn->connect_error);
-	}
+	} //Sjekke om oppkoblingen fungerer
 
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+	$username = $_POST['username']; //Henter ut brukernavn fra input-feltet på brukersiden
+	$password = $_POST['password']; //Henter ut passord fra input-feltet på brukersiden
 
-	$sql = "SELECT brukertype FROM bruker WHERE brukernavn='" . $username . "' AND passord='" . $password . "'" ;
+	$sql = "SELECT brukertype FROM bruker WHERE brukernavn='" . $username . "' AND passord='" . $password . "'" ; //Bruker variablene over for å lage sql-setningen
 
-	$login_id = $dbconn->query($sql); //Funksjon for hvordan lagre brukers fornavn fra liste bruker inn i en variabel $fornavn
+	$login_id = $dbconn->query($sql); //Henter ut svarene fra databasen, ved hjelp av sql-setningen
 
 
-	if ($login_id->num_rows > 0) {
+	if ($login_id->num_rows > 0) { //Sjekker om du får noe data returnert fra databasen
 	    // output data of each row
-	    while($row = $login_id->fetch_assoc()) {
+	    while($row = $login_id->fetch_assoc()) { //Returnerer brukertype, som er et nummer. Hvis brukertype ikke fins, får man returnert 0.
 	        echo $row["brukertype"];
 	    }
 	} else {
 	    echo "0";
 	}
-	$dbconn->close();
+	$dbconn->close(); //Lukker oppkoblingen til databasen
 
 			/*ser ntnu selv viser gammelt format så bruker det de anbefaler. Kommentert ut kode er kode for hvordan nettsider mener det skal gjøres nå. Hvis det som står over
 			fungerer - slett utkommentert kode.
