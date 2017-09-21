2b481015233ed8384437be7f99a727abb7c80340
miriamsl <miriamsl@stud.ntnu.no>
2017-09-21 09:19:22 +0200

    Fjernet en kommentar

--- database.php
+++ database.php
@@ -22,13 +22,5 @@
 	}
 	$dbconn->close(); //Lukker oppkoblingen til databasen
 
-			/*ser ntnu selv viser gammelt format så bruker det de anbefaler. Kommentert ut kode er kode for hvordan nettsider mener det skal gjøres nå. Hvis det som står over
-			fungerer - slett utkommentert kode.
-
-			$db = new mysqli("mysql.stud.ntnu.no", "it1901group8", "nullstressjoggedress", "it1901group8_festival");
-			$bandNavn = $mysqli_query($db, "SELECT" * FROM band";
-
-
-			Vi må bare se hvilken som virker */
 
 ?>
