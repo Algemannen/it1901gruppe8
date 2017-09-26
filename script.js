c59d4b0cc0af2e76330ea25863e5da4e36251e7b
Andreas Grønlien <andreas@gronlien.no>
2017-09-26 12:20:14 +0200

    ;

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
         data: {username: user.name, usertype: user.type, sceneid: scene.sid},
         type: 'post',
         success: function(output) {
-            console.log(output)
+            console.log(output);
             l = jQuery.parseJSON(output);
             console.log(l);
 
