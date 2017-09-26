1a365ecf95a79833bbd821706bb4e48f0e04882c
Andreas Grønlien <andreas@gronlien.no>
2017-09-26 12:09:48 +0200

    revert

--- script.js
+++ script.js
@@ -98,14 +98,16 @@ $(document).ready(function(){
         success: function(output) {
             l = jQuery.parseJSON(output);
             console.log(l);
-            
+
             let scenePoint = $("<li></li>").addClass("scenePoint");
             let concerts = buildListOfConcerts(bruker,l).hide();
             let buttonContainer = $("<span></span>").addClass("sceneButtonContainer");
             
-            let scene =  $("<button></button>").text(l[i].kid).addClass("scene_button");
+            let scene =  $("<button></button>").text("Dummy").addClass("scene_button");
             buttonContainer.append(scene);
             scenePoint.append(buttonContainer,concerts);
+            
+            $('scenelist').append(scenePoint);
             
             $('scenelist').append(scenePoint);
         },
