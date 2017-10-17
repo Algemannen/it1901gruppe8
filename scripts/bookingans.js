199f0d08a220fbc66f59b3120b0d127ed9cb539f
Lars Syversen <larsksy@stud.ntnu.no>
2017-10-17 13:59:35 +0200

    oppsett av søkeside for bookingansvarlig

--- scripts/bookingans.js
+++ scripts/bookingans.js
@@ -18,5 +18,31 @@ function getListOfTechnicalNeeds() {
                 console.log("Error: "+message);
             }
         }
-    });
+    })};
+
+
+function search() {
+  let l = [];
+  var inputText = $("#textinput").val();
+  var searchType = $('input[name=type]:checked').val();
+
+  $.ajax({ url: '/database.php?method=search',
+      data: {text: inputText, type: searchType},
+      type: 'post',
+      success: function(output) {
+          console.log(output);
+          l = safeJsonParse(output); //gjør en try-catch sjekk.
+          for (i in l) {
+            getTechnicalNeedsByKid(l[i].kid, l[i].navn, l[i].dato, '#tekniskebehov');
+          }
+      },
+      error: function(xmlhttprequest, textstatus, message) {
+          if(textstatus==="timeout") {
+              alert("Timeout feil, kan ikke koble til databasen");
+          } else {
+              console.log("Error: "+message);
+          }
+      }
+  });
+
 }
