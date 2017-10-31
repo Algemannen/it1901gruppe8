6a945dc4d9bfe7268330b30fdcb48b77676537e2
kronen77 <even__kj@hotmail.com>
2017-10-31 10:47:34 +0100

    oppretting av php

--- database.php
+++ database.php
@@ -1591,8 +1591,75 @@ case 'getListOfConcertDays':
       // Avslutt sql-setning
       $stmt->close();
 }
-
 break;
+
+case 'getOffersForCalender':
+    $query = "SELECT * FROM tilbud";
+
+    $stmt = $dbconn->stmt_init();
+
+    // Gjør spørringen klar for databasen
+    if(!$stmt->prepare($query)) {
+        header("HTTP/1.0 500 Internal Server Error: Failed to prepare statement.");
+    } else {
+
+      // Utfører spørringen
+      $stmt->execute();
+
+      // Returnerer resultat fra spørringen
+      $result = $stmt->get_result();
+
+      // Hent ut alle rader fra en spørring
+
+      // Returner json-string med data
+      $encode2 = array();
+      while ($row = $result2->fetch_assoc()) {
+          $encode2[] = $row;
+      }
+
+      echo json_encode($encode);
+
+      // Avslutt sql-setning
+      $stmt->close();
+  }
+break;
+case 'getConcertsForCalender':
+    $query = "SELECT * FROM konsert";
+
+    $stmt = $dbconn->stmt_init();
+
+    // Gjør spørringen klar for databasen
+    if(!$stmt->prepare($query)) {
+        header("HTTP/1.0 500 Internal Server Error: Failed to prepare statement.");
+    } else {
+
+      // Binder brukerid som heltall
+      $stmt->bind_param('i', $fid);
+
+      // Leser inn sceneid
+      $fid = $_POST['fid'];
+
+      // Utfører spørringen
+      $stmt->execute();
+
+      // Returnerer resultat fra spørringen
+      $result = $stmt->get_result();
+
+      // Hent ut alle rader fra en spørring
+
+      // Returner json-string med data
+      $encode2 = array();
+      while ($row = $result2->fetch_assoc()) {
+          $encode2[] = $row;
+      }
+
+      echo json_encode($encode);
+
+      // Avslutt sql-setning
+      $stmt->close();
+  }
+break;
+
     /// Hvis det er en skrivefeil i metodekallet så returnerer vi denne feilbeskjeden.
 default:
     header('HTTP/1.0 501 Not implemented method.');
